faa7ab966a064313f292c6aa7fed792c6b428173
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2021-01-06 17:22:03 +0530

    [Frontend] feat : Made Basic Login/ Register Page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,13 @@ import NavBar from './components/NavBar';
 import ScrumDash from './scrumsDash/ScrumDash';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginPage from './landing/LoginPage';
+import RegisterPage from './landing/RegisterPage';
+
 const theme = createMuiTheme({
 	palette: {
 		accent: {
-			primary: '#48E5C2	',
+			primary: '#48E5C2',
 			secondary: '#64F58D',
 		},
 		background: {
@@ -21,6 +24,7 @@ const theme = createMuiTheme({
 });
 export class App extends Component {
 	render() {
+		const isLoggedIn = true;
 		return (
 			<ThemeProvider theme={theme}>
 				<Router>
@@ -28,6 +32,12 @@ export class App extends Component {
 						<Route path='/scrums' exact>
 							<NavBar />
 							<ScrumDash />
+						</Route>
+						<Route path='/login' exact>
+							<LoginPage />
+						</Route>
+						<Route path='/register' exact>
+							<RegisterPage />
 						</Route>
 					</Switch>
 				</Router>
